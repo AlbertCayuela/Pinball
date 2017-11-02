bdad98b2110d7f7b42d8ccf7ff5dd2f81e63b89c
AleixCas95 <aleixca1@TR12.CAT>
2017-11-02 13:37:31 +0100

    Revert "condicions llums punts"
    This reverts commit eb22a26ab63f456f5b0985d387936ab5a2d17553.

--- Physics2_class4 - handout/ModuleSceneIntro.cpp
+++ Physics2_class4 - handout/ModuleSceneIntro.cpp
@@ -55,9 +55,9 @@ bool ModuleSceneIntro::Start()
 	sensorpuntdretaabaix2 = App->physics->CreateRectangleSensor(412, 423, 4, 4);
 	sensorpuntdretaabaix3 = App->physics->CreateRectangleSensor(422, 442, 4, 4);
 	sensorpuntadalt = App->physics->CreateRectangleSensor(177, 67, 4, 4);
-	sensorfletxaadaltesquerra = App->physics->CreateRectangleSensor(69, 26, 6, 12);
-	sensorfletxaadaltdreta = App->physics->CreateRectangleSensor(101, 26, 6, 12);
-	sensorfletxadretaabaix = App->physics->CreateRectangleSensor(18, 242, 12, 6);
+	sensorfletxaadaltesquerra = App->physics->CreateRectangleSensor(69, 26, 6, 6);
+	sensorfletxaadaltdreta = App->physics->CreateRectangleSensor(101, 26, 6, 6);
+	sensorfletxadretaabaix = App->physics->CreateRectangleSensor(18, 242, 6, 6);
 
 
 
@@ -396,16 +396,11 @@ update_status ModuleSceneIntro::Update()
 
 
 
-		if (App->player->vides <= 4 && colision == true && App->player->vides > 0 && colisionllum == true) {
+		if (App->player->vides <= 4 && colision == true && App->player->vides > 0) {
 
 			circles.add(App->physics->CreateCircle(446, 420, 30));
 			circles.getLast()->data->listener = this;
 			colision = false;
-			//Pintar aqui quan xoqui cada punt
-
-			colisionllum = false;
-
-				
 			App->player->vides--;
 
 		}
@@ -429,10 +424,6 @@ update_status ModuleSceneIntro::Update()
 	}
 
 
-	if (colisionllum == true) {
-
-		//Pintar aqui quan xoqui cada punt
-	}
 
 		/*if (App->player->vides == 3 && colision == true) {
 
@@ -569,8 +560,6 @@ void ModuleSceneIntro::OnCollision(PhysBody* bodyA, PhysBody* bodyB)
 		colisions345 = true;
 	}
 
-	if(bodyB == sensorpunt1 || bodyB == sensorpunt2 || bodyB == sensorpunt3 || bodyB == sensorpuntesquerra1 || bodyB == sensorpuntesquerra2 || bodyB == sensorpuntesquerra3 || bodyB == sensorpuntdreta1 || bodyB == sensorpuntdreta2 || bodyB == sensorpuntdreta3 || bodyB == sensorpuntdretaabaix1 || bodyB == sensorpuntdretaabaix2 || bodyB == sensorpuntdretaabaix3)
 
-		colisionllum = true;
 
 }
